639b21dd4735fc1699eae2c75f90c26e880b5473
Felipe Giron <5928944-FelpGrn@users.noreply.gitlab.com>
2021-10-15 16:17:42 +0200

    test1

---
+++ netlify/function/crawler.js
@@ -0,0 +1,45 @@
+const gql = require('graphql-tag');
+const { ApolloClient, createHttpLink, InMemoryCache } = require('@apollo/client/core');
+const fetch = require("node-fetch");
+
+const cache = new InMemoryCache()
+
+const httpLink = createHttpLink({
+    uri: 'https://api-eu-central-1.graphcms.com/v2/ckuib6o1714wi01z0bkv8ealy/master',
+    fetch
+})
+
+const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache
+})
+
+const crawlerQuery = {
+    query: gql`query {
+        products {
+          id,
+          title,
+          price
+        }
+      }`
+}
+
+exports.handler = async (event, context) => {
+    try {
+        const { data } = await apolloClient.query(crawlerQuery);
+
+        return {
+            statusCode: 200,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data.products)
+        };
+    } catch (error) {
+        console.log(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Failed fetching data' }),
+        };
+    }
+};
